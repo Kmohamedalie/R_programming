5f046ae4f06b99d9ad122c41a7b4fae06fd011dd
Mohamed Alie Kamara <63104472+Kmohamedalie@users.noreply.github.com>
2024-04-21 03:54:46 +0200

    Add files via upload

---
+++ R-command-line-functions.R
@@ -0,0 +1,30 @@
+#******************************************************************#
+#                       R command line basic                       #
+#******************************************************************#
+
+# get working directory
+getwd()
+
+# show list of directory
+list.dirs()
+
+# more details about the directory
+dir()
+
+# set working directory
+setwd(path)
+
+# create a directory
+dir.create()
+
+# show list of files in the directory
+list.files()
+
+# create a path string from directory components
+file.path()
+
+# check if file exist
+file.exists("filename.R")
+
+# create file
+file.create("filename.R")  
